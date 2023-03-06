21af966b4719d5b6914b207f10303588391ff647
종근 <114092152+why-only-english@users.noreply.github.com>
2023-03-06 21:36:32 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,9 @@
+function solution(num) {
+    var answer = 0;
+    
+    while (num !== 1 && answer !== 500) {
+        num % 2 === 0 ? num = num / 2 : num = num * 3 + 1; 
+        answer += 1;
+    }
+    return num === 1 ? answer : -1;
+}
