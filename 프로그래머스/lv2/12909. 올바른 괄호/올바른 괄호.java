e2e5fc0e882985c1a44b0d4c3d8e299d06c3d182
종근 <114092152+why-only-english@users.noreply.github.com>
2023-03-10 16:44:59 +0900

    [level 2] Title: 올바른 괄호, Time: 20.32 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push('1');
+            } else if (s.charAt(i) == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
