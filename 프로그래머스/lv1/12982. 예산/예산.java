78075eae6e348e1104062f194ca90f916577417c
종근 <114092152+why-only-english@users.noreply.github.com>
2023-03-14 17:13:51 +0900

    [level 1] Title: 예산, Time: 0.34 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int sum = 0;
+        
+        Arrays.sort(d);
+        // System.out.println(Arrays.toString(d));
+        
+        for (int i = 0; i < d.length; i++) {
+            sum += d[i];
+            if(sum > budget) {
+                answer = i;
+                break;
+            }
+        }
+        if (sum <= budget) {
+            answer = d.length;
+        }
+        return answer;
+    }
+}
