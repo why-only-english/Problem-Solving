794c50459cb3b3834fd1cc1f33d8d5fbb48fc4e0
종근 <114092152+why-only-english@users.noreply.github.com>
2024-01-05 13:17:41 +0900

    [Silver I] Title: 부분수열의 합, Time: 644 ms, Memory: 69364 KB -BaekjoonHub

---
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static int[] arr;
+    static Set<Integer> possibleSum = new HashSet<>();
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(0, 0);
+
+        // 나올 수 없는 가장 작은 자연수 구하기
+        for (int i = 1; ; i++) {
+            if(!possibleSum.contains(i)) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+    public static void dfs(int current, int sum) {
+        if (current == N) {
+            // 구해진 값 체크
+            possibleSum.add(sum);
+            return;
+        }
+
+        dfs(current + 1, sum + arr[current]);
+        dfs(current + 1, sum);
+    }
+}
