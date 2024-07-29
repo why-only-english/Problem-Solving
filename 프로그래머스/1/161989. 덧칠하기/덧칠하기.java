93889b88c61aafaf20d824da407675c000b2b211
JONG GEUN <114092152+why-only-english@users.noreply.github.com>
2024-07-29 17:33:15 +0900

    [level 1] Title: 덧칠하기, Time: 5.25 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int roller = section[0];
+        int cnt = 1;
+        for(int i = 1; i < section.length; i++) {
+                // System.out.println(section[i]);
+            if(roller + m - 1 < section[i]) {
+                cnt++;
+                roller = section[i];
+            }
+        }
+        return cnt;
+    }
+}
